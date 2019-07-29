a80bbaf23382a8c9d8feb19442ef3301c88d69b5
dianevinson <43148083+dianevinson@users.noreply.github.com>
2019-07-29 11:53:15 -0400

    Update app.js

--- practice/app.js
+++ practice/app.js
@@ -1,3 +1,11 @@
-db.collection('friends').get().then( (snapshot) => {
-  console.log(snapshot.docs);
+db.collection("friends").add({
+    name: "Jordan",
+    relationship: "friend",
+    height: 6.2
 })
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
