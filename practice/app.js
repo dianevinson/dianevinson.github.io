a46dcba079b71381c6d141b79269fcf4ad0bde88
dianevinson <43148083+dianevinson@users.noreply.github.com>
2019-07-29 12:54:28 -0400

    Update app.js

--- practice/app.js
+++ practice/app.js
@@ -1,3 +1,4 @@
+const friendList = document.querySelector("#friend-list")
 db.collection('friends').get().then((snapshot) => {
         snapshot.docs.forEach( doc => {
                 console.log(doc.data())
