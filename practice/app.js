137221cfafef6f6a911501d5e6c905c8b32379dc
dianevinson <43148083+dianevinson@users.noreply.github.com>
2019-07-29 12:14:54 -0400

    Update app.js

--- practice/app.js
+++ practice/app.js
@@ -7,20 +7,3 @@ var firebaseConfig = {
         messagingSenderId: "102580477557",
         appId: "1:102580477557:web:cb01afadbdb8d465"
       };
-firebase.initializeApp(firebaseConfig);
-db.collection("friends").add({
-    name: "Jordan",
-    relationship: "friend",
-    height: 6.2
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
-db.collection("friends").get().then((querySnapshot) => {
-    querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`);
-    });
-});
