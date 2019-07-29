a9acb67b614769e9fba1e81c22e1c6c5b464c32e
dianevinson <43148083+dianevinson@users.noreply.github.com>
2019-07-29 13:07:42 -0400

    Update app.js

--- practice/app.js
+++ practice/app.js
@@ -1,6 +1,22 @@
-const friendList = document.querySelector("#friend-list")
+const friendList = document.querySelector("#friend-list");
+//create element and render cafe
+function renderFriend(doc) {
+        let li = document.createElement('li');
+        let name = document.createElement('span');
+        let relationship = document.createElement('span');
+        
+        li.setAttribute('data-id', doc.id);
+        name.textContent = doc.data().name;
+        relationship.textContent = doc.data().relationship
+        
+        li.appendChild(name);
+        li.appendChild(relationship);
+        
+        friendList.appendChild(li);
+}
+
 db.collection('friends').get().then((snapshot) => {
         snapshot.docs.forEach( doc => {
-                console.log(doc.data())
+                renderFriend(doc)
         })
 })
