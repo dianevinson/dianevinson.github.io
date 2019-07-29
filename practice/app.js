b339d17c53ee34b2f7d403cbf7c1eefbf0c4b29b
dianevinson <43148083+dianevinson@users.noreply.github.com>
2019-07-29 12:32:49 -0400

    Update app.js

--- practice/app.js
+++ practice/app.js
@@ -7,7 +7,15 @@ var firebaseConfig = {
         messagingSenderId: "102580477557",
         appId: "1:102580477557:web:cb01afadbdb8d465"
       };
+
 // Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: 'AIzaSyAv9PBlsB68imhqyvec2Cg8nR0HPytrTy0',
+  authDomain: 'practice-proj-88203.firebaseapp.com',
+  projectId: 'practice-proj-88203'
+});
+
+var db = firebase.firestore();
 
 db.collection('friends').get().then((snapshot) => {
         console.log(snapshot.docs);
