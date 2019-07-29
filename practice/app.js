7e8fd3d46d39394d388619add02ce89b4ad7cf1b
dianevinson <43148083+dianevinson@users.noreply.github.com>
2019-07-29 12:18:22 -0400

    Update app.js

--- practice/app.js
+++ practice/app.js
@@ -15,3 +15,14 @@ firebase.initializeApp({
 });
 
 var db = firebase.firestore();
+db.collection("friends").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
